eba779075340104436d1067e861cbf010a8b8e3b
Cevdet Gulser <c.gulser86@gmail.com>
2022-05-15 01:10:30 +0200

    14.05.2022

--- src/test/java/day06_radioButton_checkBox/C05_Deneme.java
+++ src/test/java/day06_radioButton_checkBox/C05_Deneme.java
@@ -1,14 +1,20 @@
 package day06_radioButton_checkBox;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+
 import java.time.Duration;
-public class C03_RadioButton {
+
+public class C05_Deneme {
     WebDriver driver;
     //1. Bir class oluşturun : RadioButtonTest
     //2. Gerekli yapiyi olusturun ve aşağıdaki görevi tamamlayın.
@@ -20,18 +26,20 @@ public class C03_RadioButton {
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
     }
     @After
-    public void tearDown(){
-        driver.close();
-    }
+    public void tearDown(){ driver.close();}
     @Test
     public void test01() throws InterruptedException {
         //https://www.facebook.com adresine gidin
         driver.get("https://www.facebook.com");
+        Actions action = new Actions(driver);
+        action.sendKeys(Keys.PAGE_DOWN).build().perform();
+
 
         //Cookies’i kabul edin
-        driver.findElement(By.xpath("//*[text()='Allow essential and optional cookies']")).click();
+        driver.findElement(By.xpath("//button[@title='Nur erforderliche Cookies erlauben']")).click();
+
         //“Create an Account” button’una basin
-        driver.findElement(By.xpath("//*[text()='Create new account']")).click();
+        driver.findElement(By.xpath("//a[@rel='async']")).click();
 
         //“radio buttons” elementlerini locate edin
         WebElement femaleButton=driver.findElement(By.xpath("//input[@value='1']"));
@@ -44,7 +52,5 @@ public class C03_RadioButton {
             maleButton.click();
         }
         Thread.sleep(3000);
-
-   //---> Bu Class'taki hatalar C06_Deneme class'inda giderildi <---//
     }
 }
