ca9bbe21967db25b370b1a61d6152c4a42a8509b
Cevdet Gulser <c.gulser86@gmail.com>
2022-05-28 01:09:08 +0200

    28.05.2022

--- src/test/java/day15_writeExcel_screenShot/C04_JS_Executor.java
+++ src/test/java/day15_writeExcel_screenShot/C04_JS_Executor.java
@@ -11,8 +11,7 @@ public class C04_JS_Executor extends TestBase {
         driver.get("https://www.amazon.com");
         // asagidaki sign in butonu gorununceye kadar js ile scrool yapalim
         JavascriptExecutor jse= (JavascriptExecutor) driver;
-
-        WebElement signInButonu= driver.findElement(By.xpath("(//span[text()='Anmelden'])[3]"));
+        WebElement signInButonu= driver.findElement(By.xpath("(//span[text()='Anmelden'])[2]"));
         Thread.sleep(3000);
         jse.executeScript("arguments[0].scrollIntoView(true);",signInButonu);
         // sign in butonuna js ile click yapalim
